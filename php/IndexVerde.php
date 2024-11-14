7051e60a1e8a6d268ea0b95e2d5dbb1d488ed3ae
Fabio Cavazzini <fabio.cavazzini@iticopernico.it>
2024-11-14 17:58:57 +0100

    Change Green

--- php/IndexVerde.php
+++ php/IndexVerde.php
@@ -15,26 +15,26 @@
     <head>
         <link rel="stylesheet" type="text/css" media="all" href="../css/CssIndexVerde.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Percorso4</title>
+        <title>Percorso Gruppo Verde</title>
         <script>
-function cambiaeInvia()
-{
-var i,j;
-var arrivato = document.forms[0];
-for (i = 0; i < arrivato.length; i++)
-{
-if (arrivato[i].checked)
-{
-j=i;
-}
-}
+          function cambiaeInvia()
+          {
+            var i,j;
+            var arrivato = document.forms[0];
+            for (i = 0; i < arrivato.length; i++)
+            {
+              if (arrivato[i].checked)
+              {
+              	j=i;
+              }
+            }
 
-document.getElementById("id"+j).disabled = true;
-// document.forms[0].submit();
+            document.getElementById("id"+j).disabled = true;
+            // document.forms[0].submit();
 
-document.getElementById("p"+j).style.textDecoration = "line-through";
-}
-</script>
+            document.getElementById("p"+j).style.textDecoration = "line-through";
+          }
+        </script>
     </head>
 
     <body>
@@ -46,13 +46,14 @@ document.getElementById("p"+j).style.textDecoration = "line-through";
             
             <div id = "Questinario">
                 <form action="pagina_invisibile2.php" method="post">
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id0"> <span class = "ModTesto" id="p0">Elettrotecnica (OE secondo piano) [5minuti]</span><br>
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id1"> <span class = "ModTesto" id="p1">Meccanica /Energia (capannone) [5minuti+5minuti]</span><br>
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id2"> <span class = "ModTesto" id="p2">Chimica /Biotec (lab. 38/ACH) [5minuti+5minuti]</span><br>
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id3"> <span class = "ModTesto" id="p3">Ottico (lab 12) [5minuti]</span><br>
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id4"> <span class = "ModTesto" id="p4">Moda (aula 2) [5minuti]</span><br>
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id5"> <span class = "ModTesto" id="p5">Informatica (lab. 19) [5minuti+5minuti]</span><br>
-                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id6"> <span class = "ModTesto" id="p6">Elettronica/Telecom (lab. 16/17) [5minuti+5minuti]</span><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id0"> <span class = "ModTesto" id="p0"><b>Elettrotecnica</b><br>OE secondo piano<br>5 minuti</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id1"> <span class = "ModTesto" id="p1"><b>Meccanica/Energia</b><br>lab. 49/Officina Meccanica<br>5 miinuti + 5 minuti</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id2"> <span class = "ModTesto" id="p2"><b>Chimica/Biotecnologie</b><br>lab. 38/ACH<br>tot 10 min</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id3"> <span class = "ModTesto" id="p3"><b>Ottica</b><br>lab. 11<br>5 minuti</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id4"> <span class = "ModTesto" id="p4"><b>Moda</b><br>aula 02<br>5 minuti</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id5"> <span class = "ModTesto" id="p5"><b>Corso Quadriennale</b><br>aula 10<br>5 minuti</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id6"> <span class = "ModTesto" id="p6"><b>Informatica</b><br>lab. 19<br>5 minuti</span><br><br><br>
+                    <input onchange="cambiaeInvia()" type="radio" class="ModCheckbox" name="Rad" id="id7"> <span class = "ModTesto" id="p7"><b>Elettronica/Telecomunicazioni</b><br>lab. 16/17<br>5 minuti</span>
                 </form>
             </div>
         </div>    
